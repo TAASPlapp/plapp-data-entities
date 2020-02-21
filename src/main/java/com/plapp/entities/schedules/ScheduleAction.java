781fd342dbd72b5e04aeecfdb315d68248c384fc
Carlo Alberto Barbano <carlo.alberto.barbano@outlook.com>
2020-02-21 16:32:38 +0100

    add userId & setters

--- src/main/java/com/plapp/entities/schedules/ScheduleAction.java
+++ src/main/java/com/plapp/entities/schedules/ScheduleAction.java
@@ -3,21 +3,24 @@ package com.plapp.entities.schedules;
 import java.util.Date;
 
 public class ScheduleAction {
-    private long plantID;
+    private long userId;
+    private long plantId;
     private String action;
     private Date date;
     private int periodicity;
 
 
-    public ScheduleAction(long plantID, Date date, String action, int periodicity) {
-        this.plantID = plantID;
+    public ScheduleAction(long plantId, Date date, String action, int periodicity) {
+        this.plantId = plantId;
         this.date = date;
         this.action = action;
         this.periodicity = periodicity;
     }
 
-    public long getPlantID() {
-        return plantID;
+    public long getUserId() { return userId; }
+
+    public long getPlantId() {
+        return plantId;
     }
 
     public Date getDate() {
@@ -35,10 +38,30 @@ public class ScheduleAction {
     @Override
     public String toString() {
         return "ScheduleAction{" +
-                ", plantID=" + plantID +
+                ", plantId=" + plantId +
                 ", date=" + date +
                 ", action='" + action +
                 ", periodicity='" + periodicity + '\'' +
                 '}';
+    }
+
+    public void setUserId(long userId) {
+        this.userId = userId;
+    }
+
+    public void setPlantId(long plantId) {
+        this.plantId = plantId;
+    }
+
+    public void setAction(String action) {
+        this.action = action;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    public void setPeriodicity(int periodicity) {
+        this.periodicity = periodicity;
     }
 }
