b519fab06201e8a855df900ce1df269aa45c5d23
Carlo Alberto Barbano <carlo.alberto.barbano@outlook.com>
2020-02-21 18:37:01 +0100

    Add UserDetails

---
+++ src/main/java/com/plapp/entities/social/UserDetails.java
@@ -0,0 +1,60 @@
+package com.plapp.entities.social;
+
+import java.util.Date;
+
+public class UserDetails {
+    private String username;
+    private String firstName;
+    private String lastName;
+    private String bio;
+    private Date birthdate;
+    private String profilePicture;
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getBio() {
+        return bio;
+    }
+
+    public void setBio(String bio) {
+        this.bio = bio;
+    }
+
+    public Date getBirthdate() {
+        return birthdate;
+    }
+
+    public void setBirthdate(Date birthdate) {
+        this.birthdate = birthdate;
+    }
+
+    public String getProfilePicture() {
+        return profilePicture;
+    }
+
+    public void setProfilePicture(String profilePicture) {
+        this.profilePicture = profilePicture;
+    }
+}
