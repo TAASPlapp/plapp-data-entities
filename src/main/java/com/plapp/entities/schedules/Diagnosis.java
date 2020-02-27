260ab0869722c4d4c0d8bb2b2a4868396ea9318d
gegelumis <37594043+gegelumis@users.noreply.github.com>
2020-02-27 18:17:34 +0100

    Add files via upload

---
+++ src/main/java/com/plapp/entities/schedules/Diagnosis.java
@@ -0,0 +1,52 @@
+package com.plappgardenerservice.entities;
+
+
+public class Diagnosis {
+
+
+    private String plantID;
+    private boolean ill;
+    private String disease;
+
+    public Diagnosis(){}
+
+    public Diagnosis(String plantID, boolean ill, String disease) {
+        this.plantID = plantID;
+        this.ill = ill;
+        this.disease = disease;
+    }
+
+
+    public String getPlantID() {
+        return plantID;
+    }
+
+    public void setPlantID(String plantID) {
+        this.plantID = plantID;
+    }
+
+    public boolean isIll() {
+        return ill;
+    }
+
+    public void setIll(boolean ill) {
+        this.ill = ill;
+    }
+
+    public String getDisease() {
+        return disease;
+    }
+
+    public void setDisease(String disease) {
+        this.disease = disease;
+    }
+
+    @Override
+    public String toString() {
+        return "Diagnosis{" +
+                "plantID='" + plantID + '\'' +
+                ", ill=" + ill +
+                ", disease='" + disease + '\'' +
+                '}';
+    }
+}
