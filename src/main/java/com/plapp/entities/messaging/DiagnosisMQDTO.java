e120247b8ea2c746de39ac9c95be1bb9fb9f1600
gegelumis <37594043+gegelumis@users.noreply.github.com>
2020-03-03 21:14:04 +0100

    DiagnosisMQDTO

---
+++ src/main/java/com/plapp/entities/messaging/DiagnosisMQDTO.java
@@ -0,0 +1,50 @@
+package com.plapp.entities.messaging;
+
+import com.plapp.entities.greenhouse.Plant;
+
+public class DiagnosisMQDTO {
+
+    private long userId;
+    private Plant plant;
+    private boolean ill;
+    private String disease;
+
+    public DiagnosisMQDTO(long userId, Plant plant, boolean ill, String disease) {
+        this.userId = userId;
+        this.plant = plant;
+        this.ill = ill;
+        this.disease = disease;
+    }
+
+    public long getUserId() {
+        return userId;
+    }
+
+    public void setUserId(long userId) {
+        this.userId = userId;
+    }
+
+    public Plant getPlant() {
+        return plant;
+    }
+
+    public void setPlant(Plant plant) {
+        this.plant = plant;
+    }
+
+    public boolean isIll() {
+        return ill;
+    }
+
+    public void setIll(boolean ill) {
+        this.ill = ill;
+    }
+
+    public String getDisease() {
+        return disease;
+    }
+
+    public void setDisease(String disease) {
+        this.disease = disease;
+    }
+}
