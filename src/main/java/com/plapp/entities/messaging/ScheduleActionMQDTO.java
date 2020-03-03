ae41826470c11251530a4cdd23debccbe8aa0c21
Carlo Alberto Barbano <carlo.alberto.barbano@outlook.com>
2020-03-03 20:55:50 +0100

    add ScheduleActionMQDTO

---
+++ src/main/java/com/plapp/entities/messaging/ScheduleActionMQDTO.java
@@ -0,0 +1,61 @@
+package com.plapp.entities.messaging;
+
+import com.plapp.entities.greenhouse.Plant;
+import java.util.Date;
+
+public class ScheduleActionMQDTO {
+    private long userId;
+    private Plant plant;
+    private String action;
+    private Date date;
+    private int periodicity;
+    private String additionalInfo;
+
+    public long getUserId() {
+        return userId;
+    }
+
+    public void setUserId(long userId) {
+        this.userId = userId;
+    }
+
+    public Plant getPlant() {
+        return plant;
+    }
+
+    public void setPlant(Plant plant) {
+        this.plant = plant;
+    }
+
+    public String getAction() {
+        return action;
+    }
+
+    public void setAction(String action) {
+        this.action = action;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    public int getPeriodicity() {
+        return periodicity;
+    }
+
+    public void setPeriodicity(int periodicity) {
+        this.periodicity = periodicity;
+    }
+
+    public String getAdditionalInfo() {
+        return additionalInfo;
+    }
+
+    public void setAdditionalInfo(String additionalInfo) {
+        this.additionalInfo = additionalInfo;
+    }
+}
